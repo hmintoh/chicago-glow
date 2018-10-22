112ba1868f78383d8d5915379a7d0ea06fa5c370
huimintoh <hmintoh@gmail.com>
2018-10-23 00:23:49 +0800

    to add static navbar. wip styling

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { HashRouter as Router, NavLink, Redirect } from "react-router-dom";
+import logo from "../assets/cgw logo.png";
+import "./routes.css";
 
 const Routes = () => {
   return (
     <div>
       <Router>
         <div>
-          <h1>chicago global wealth</h1>
+          <img className="logo" src={logo} alt="logo" />
           <ul className="navbar">
             <li><NavLink to="/">Home</NavLink></li>
             <li><NavLink to="/about">About</NavLink></li>
             <li><NavLink to="/offerings">Offerings</NavLink></li>
             <li><NavLink to="/team">Team</NavLink></li>
             <li><NavLink to="/contact">Contact</NavLink></li>
+            <li><a href="https://www.chicagoglobalwealth.com/">BLOG</a></li>
           </ul>
         </div>
       </Router>
-    </div>
+    </div >
   )
 }
 

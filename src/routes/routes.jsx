f0fbb4d7b409d77391dc980a2cbc74b8ee41d459
huimintoh <hmintoh@gmail.com>
2018-10-26 00:14:45 +0800

    to add skeletonz AdvisoryBoard component. wip styling

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, NavLink, Redirect } from "react-router-dom";
+import {HashRouter as Router, NavLink, Redirect} from "react-router-dom";
 import logo from "../assets/cgw logo.png";
 import "./routes.css";
 
@@ -10,17 +10,32 @@ const Routes = () => {
         <div>
           <img className="logo" src={logo} alt="logo" />
           <ul className="navbar">
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/about">About</NavLink></li>
-            <li><NavLink to="/offerings">Offerings</NavLink></li>
-            <li><NavLink to="/team">Team</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
-            <li><a href="https://www.chicagoglobalwealth.com/">BLOG</a></li>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/offerings">Offerings</NavLink>
+            </li>
+            <li>
+              <NavLink to="/team">Team</NavLink>
+            </li>
+            <li>
+              <NavLink to="/advisory">Advisory</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+            <li>
+              <a href="https://www.chicagoglobalwealth.com/">BLOG</a>
+            </li>
           </ul>
         </div>
       </Router>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default Routes
+export default Routes;

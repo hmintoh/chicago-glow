cb9b49db9595002534ad7c7ee92f3561a404ba55
huimintoh <hmintoh@gmail.com>
2018-11-02 18:42:20 +0800

    to add text content in about and research components

--- src/research/research.jsx
+++ src/research/research.jsx
@@ -10,19 +10,31 @@ const Research = () => {
           <div className="services_item">
             <h3>Index Investing</h3>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              Fewer than 15% of large-cap funds beat the S&P 500 in the last 10
+              years. Poor performance brought spotlight on fees, which became
+              hard to justify. Many active managers are really closet indexers,
+              holding most stocks in their benchmarks.
             </p>
+            <button className="btn_more">LEARN MORE</button>
           </div>
           <div className="services_item">
             <h3>Efficient Markets</h3>
-            <button className="btn_readMore">READ MORE</button>
+            <p>
+              Investors spend $100 billion per year trying to beat the market.
+              Decades of data prove index strategies do better over the long
+              term. We have developed a product that addresses this issue and
+              lets you take advantage of Chicago-style investing.
+            </p>
+            <button className="btn_more">LEARN MORE</button>
           </div>
           <div className="services_item">
             <h3>Behavioral Finance</h3>
-            <button className="btn_readMore1">READ MORE</button>
+            <p>
+              Due to attempts at market timing and poor behavioral finance
+              choices (selling after a market correction), the average investor
+              actually underperforms the overall market.
+            </p>
+            <button className="btn_more">LEARN MORE</button>
           </div>
         </div>
       </div>

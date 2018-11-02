94f9ef97bfd57ccc088c635348bbc76872b9d1ee
huimintoh <hmintoh@gmail.com>
2018-11-03 00:19:12 +0800

    to add jumbotron image placeholder

--- src/jumbotron/jumbotron.jsx
+++ src/jumbotron/jumbotron.jsx
@@ -4,9 +4,11 @@ import "./jumbotron.css";
 const Jumbotron = () => {
   return (
     <div className="secA jumbotron">
-      <h1 className="tagline">Reshaping Wealth Management</h1>
+      <h1 className="animated fadeIn slow delay-1s tagline">
+        Reshaping Wealth Management
+      </h1>
     </div>
-  )
-}
+  );
+};
 
 export default Jumbotron;

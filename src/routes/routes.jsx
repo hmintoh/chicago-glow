5af771875f04c4dee8bc30acb266dee776610b74
huimintoh <hmintoh@gmail.com>
2018-12-26 16:01:37 +0800

    add function to hide nevbar when scrolling on small devices

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,55 +1,87 @@
-import React from "react";
+import React, {Component} from "react";
 import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
 import logo_horizontal from "../assets/cgw_logo+text+horizontal.png";
 import Home from "../home";
-
 import "./routes.css";
 
-const Routes = () => {
-  return (
-    <div>
-      <Router>
-        <div>
-          <div className="nav">
-            <Link to="/">
-              <img className="logo" src={logo_horizontal} alt="logo" />
-            </Link>
-            <ul className="navbar">
-              <li>
-                <a className="navlinks" href="#about">
-                  About
-                </a>
-              </li>
-              <li>
-                <a className="navlinks" href="#principles">
-                  Principles
-                </a>
-              </li>
-              <li>
-                <a className="navlinks" href="#team">
-                  Team
-                </a>
-              </li>
-              <li>
-                <a className="navlinks" href="#research">
-                  Research
-                </a>
-              </li>
-              <li>
-                <a className="navlinks" href="#contact">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
+let lastScrollTop = 0;
 
-          <Switch>
-            <Route exact path="/" component={Home} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  );
-};
+class Routes extends Component {
+  constructor() {
+    super();
+    this.state = {showMenu: true};
+  }
+
+  componentDidMount = () => {
+    window.innerWidth < 500 &&
+      window.addEventListener("scroll", this.handleScroll);
+  };
+
+  componentWillUnmount = () => {
+    window.removeEventListener("scroll", this.handleScroll);
+  };
+
+  handleScroll = event => {
+    const currentScrollTop = window.pageYOffset;
+
+    if (currentScrollTop > lastScrollTop) {
+      document.getElementById("nav").style.display = "none";
+    } else {
+      document.getElementById("nav").style.display = "block";
+    }
+    lastScrollTop = currentScrollTop;
+  };
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <div>
+            <div
+              id="nav"
+              className="animated fadeInDown"
+              onScroll={this.handleScroll}
+            >
+              <Link to="/">
+                <img className="logo" src={logo_horizontal} alt="logo" />
+              </Link>
+              <ul className="navbar">
+                <li>
+                  <a className="navlinks" href="#about">
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a className="navlinks" href="#principles">
+                    Principles
+                  </a>
+                </li>
+                <li>
+                  <a className="navlinks" href="#team">
+                    Team
+                  </a>
+                </li>
+                <li>
+                  <a className="navlinks" href="#research">
+                    Research
+                  </a>
+                </li>
+                <li>
+                  <a className="navlinks" href="#contact">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
 
 export default Routes;

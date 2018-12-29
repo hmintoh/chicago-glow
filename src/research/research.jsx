0ae1f133106856d1a2d05b97f007583046a78e50
huimintoh <hmintoh@gmail.com>
2018-12-30 01:47:54 +0800

    update research content and assets

--- src/research/research.jsx
+++ src/research/research.jsx
@@ -1,8 +1,8 @@
 import React, {Component} from "react";
 import ReactModal from "react-modal";
-import bh from "./bh.png";
-import ii from "./ii.png";
-import em from "./em.png";
+import eit from "./ExcessiveInvestorTrading.png";
+import df from "./DividendsFallacy.png";
+import rma from "./RollingMentalAccounts.png";
 import "./research.css";
 
 ReactModal.setAppElement("#root");
@@ -19,6 +19,36 @@ const customStyles = {
   }
 };
 
+const researchBlurb = [
+  {
+    title: "Excessive Investor Trading",
+    blurb:
+      "Both individual investors and mutual funds do not view each stock individually, they compare positions across their portfolio and sell their biggest winners and losers.",
+    link: "https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2503987",
+    details:
+      "I document a new stylized fact about how investors trade assets: individuals are more likely to sell the extreme winning and extreme losing positions in their portfolio (“the rank effect”). This effect is not driven by firm-specific information, holding period or the level of returns itself, but is associated with the salience of extreme portfolio positions. The rank effect is exhibited by both retail traders and mutual fund managers. The effect indicates that trades in a given stock depend on how it compares to other positions in an investor’s portfolio.",
+    image: eit
+  },
+  {
+    title: "Dividends Fallacy",
+    blurb:
+      "Investors trade as if they consider dividends and capital gains in separate mental accounts, without fully appreciating that dividends come at the expense of price decreases - the free dividends fallacy.",
+    link: "https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2876373",
+    details:
+      "Many individual investors, mutual funds and institutions trade as if dividends and capital gains are disconnected attributes, not fully appreciating that dividends result in price decreases. Behavioral trading patterns (e.g. the disposition effect) are driven by price changes instead of total returns. Investors rarely reinvest dividends, and trade as if they are a separate, stable income stream. Analysts fail to account for the effect of dividends on price, leading to optimistic price forecasts for dividend-paying stocks. Demand for dividends is systematically higher in periods of low interest rates and poor market performance, leading to lower returns for dividend-paying stocks.",
+    image: df
+  },
+  {
+    title: "Rolling Mental Accounts Fallacy",
+    blurb:
+      "Mental accounts are not always closed when an investor sells a stock - a sale does not always conclude an investing episode. Instead, investors may 'roll' an account from one asset to another, by selling the original asset and buying another within a short period of time.",
+    link: "https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2653929",
+    details:
+      "When investors sell one asset and quickly buy another (“reinvestment days”), their trades suggest the original mental account is not closed, but is instead rolled into the new asset. Investors display a rolled disposition effect, selling the new position when its value exceeds the investment in the original position. On reinvestment days, investors display no disposition effect (consistent with no disutility from realizing a loss) and make better selling decisions. Mutual funds exhibit a larger disposition effect when outflows prevent them from rolling accounts. Using a laboratory experiment, we show that reinvestment causally reduces the disposition effect and improves trading.",
+    image: rma
+  }
+];
+
 ReactModal.setAppElement("#root");
 
 class Research extends Component {
@@ -27,16 +57,8 @@ class Research extends Component {
     this.state = {showModal: false, title: ""};
   }
 
-  handleIndexModal = () => {
-    this.setState({showModal: true, title: "Index Investing"});
-  };
-
-  handleEfficientxModal = () => {
-    this.setState({showModal: true, title: "Efficient Markets"});
-  };
-
-  handleBehavioralModal = () => {
-    this.setState({showModal: true, title: "Behavioral Finance"});
+  handleOpenModal = e => {
+    this.setState({showModal: true, title: e.target.name});
   };
 
   handleCloseModal = () => {
@@ -44,53 +66,51 @@ class Research extends Component {
   };
 
   modalToShow = title => {
-    if (title === "Index Investing") {
+    if (title === "Excessive Investor Trading") {
       return (
         <div className="animated slow fadeIn modal-grid">
           <div className="modal-item">
             <h3>{title}</h3>
-            <p>
-              Fewer than 15% of large-cap funds beat the S&P 500 in the last 10
-              years. Poor performance brought spotlight on fees, which became
-              hard to justify. Many active managers are really closet indexers,
-              holding most stocks in their benchmarks.
-            </p>
+            <p>{researchBlurb[0].details}</p>
           </div>
           <div className="modal-item">
-            <img src={ii} className="modal_item research_images" alt="ii" />
+            <img
+              src={researchBlurb[0].image}
+              className="modal_item research_images"
+              alt="eit"
+            />
           </div>
         </div>
       );
-    } else if (title === "Efficient Markets") {
+    } else if (title === "Dividends Fallacy") {
       return (
         <div className="animated slow fadeIn modal-grid">
           <div className="modal-item">
             <h3>{title}</h3>
-            <p>
-              Investors spend $100 billion per year trying to beat the market.
-              Decades of data prove index strategies do better over the long
-              term. We have developed a product that addresses this issue and
-              lets you take advantage of Chicago-style investing.
-            </p>
+            <p>{researchBlurb[1].details}</p>
           </div>
           <div className="modal-item">
-            <img src={em} className="research_images" alt="em" />
+            <img
+              src={researchBlurb[1].image}
+              className="research_images"
+              alt="df"
+            />
           </div>
         </div>
       );
-    } else if (title === "Behavioral Finance") {
+    } else if (title === "Rolling Mental Accounts Fallacy") {
       return (
         <div className="animated slow fadeIn modal-grid">
           <div className="modal-item">
             <h3>{title}</h3>
-            <p>
-              Due to attempts at market timing and poor behavioral finance
-              choices (selling after a market correction), the average investor
-              actually underperforms the overall market.
-            </p>
+            <p>{researchBlurb[2].details}</p>
           </div>
           <div className="modal-item">
-            <img src={bh} className="research_images" alt="bh" />
+            <img
+              src={researchBlurb[2].image}
+              className="research_images"
+              alt="rma"
+            />
           </div>
         </div>
       );
@@ -105,49 +125,31 @@ class Research extends Component {
         <div className="container">
           <h2>Featured Research</h2>
           <div className="services_grid">
-            <div className="services_item">
-              <h3>Low Fees</h3>
-              <p>
-                Investors pay over $100 billion a year in management fees. Fee
-                reduction is the purest outperformance that funds can deliver to
-                investors. Our quantitative investment approach that allows us
-                to analyze over 30,000 stocks globally at minimal costs without
-                sacrificing performance.
-              </p>
-              <button className="btn_more" onClick={this.handleIndexModal}>
-                Learn More
-              </button>
-            </div>
-            <div className="services_item">
-              <h3>Long Term Efficient</h3>
-              <p>
-                Over the long term markets reflect fundamentals, but even Eugene
-                Fama does not believe that markets are efficient always. Our
-                disciplined strategies are refined from Chicago research based
-                on over 50 years of data across over 20 countries to take
-                advantage of both short-term errors and long-term corrections.
-              </p>
-              <button className="btn_more" onClick={this.handleEfficientxModal}>
-                Learn More
-              </button>
-            </div>
-            <div className="services_item">
-              <h3>Behavioral Finance</h3>
-              <p>
-                Individual investors tend to chase returns and overreact to
-                short-term news. We exploit these short-term errors in the
-                market while striving to reduce behavioral biases among our own
-                investors through the application of Richard Thaler’s principles
-                using nudges and sludges.
-              </p>
-              <button className="btn_more" onClick={this.handleBehavioralModal}>
-                Learn More
-              </button>
-            </div>
+            {researchBlurb.map((item, key) => (
+              <div key={key} className="services_item">
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.blurb}</p>
+                </div>
+                <div className="research_btns_container">
+                  <a href={item.link} target="_blank" rel="noopener noreferrer">
+                    <button className="btn_more">See Full Paper</button>
+                  </a>
+                  <button
+                    name={item.title}
+                    className="btn_more"
+                    onClick={this.handleOpenModal}
+                  >
+                    Learn More
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
+
           <ReactModal isOpen={showModal} style={customStyles}>
             {this.modalToShow(title)}
-            <button className="btn_more" onClick={this.handleCloseModal}>
+            <button className="btn_close" onClick={this.handleCloseModal}>
               Close
             </button>
           </ReactModal>

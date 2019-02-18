6e286d7d1663ab2113f7f54c52601ff3297366fb
huimintoh <hmintoh@gmail.com>
2019-02-18 18:33:05 +0800

    refactor(Contact): update email address

--- src/contact/contact.jsx
+++ src/contact/contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
     <div id="contact" className="secB">
       <div className="container">
         <h2>Get in Touch</h2>
-        <p>Ivan.Chelebiev@mindfulwealth.com</p>
+        <p>chicago.global@mindfulwealth.com</p>
         <a
           href="https://www.mindfulwealth.com/en/fund-management/chicago-global-strategies/"
           target="_blank"
